254e129cd2e5c2a59363856a287ba32e0129444c
JosephCarrino <josephcarrinospam@gmail.com>
2026-06-08 12:10:06 +0200

    SIAM OP26 talk - added post and talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,5 +1,15 @@
 var addressPoints = [
   [
+    "Presentation at SIAM OP26<br />PEQUAN Team meeting; University of Edinburgh, Edinburgh, UK",
+    55.9440765,
+    -3.1883736
+  ],
+  [
+    "Talk at PEQUAN Team meeting<br />PEQUAN Team meeting; Sorbonne University, Paris, France",
+    48.8468997,
+    2.3574487
+  ],
+  [
     "Poster on 'Mixed-Precision Newton's method' at SIAM Chapters Meeting<br />SIAM Chapters meeting for Young Researchers; PoliMi, Milan, Italy",
     45.4800101,
     9.2282387
@@ -8,10 +18,5 @@ var addressPoints = [
     "Poster on 'Mixed-Precision Newton's method' at M2L<br />M2L Summer School; University of Split, Split, Croatia",
     43.5117478,
     16.4676015
-  ],
-  [
-    "Talk at PEQUAN Team meeting<br />PEQUAN Team meeting; Sorbonne University, Paris, France",
-    48.8468997,
-    2.3574487
   ]
 ];
